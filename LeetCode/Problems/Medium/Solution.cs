7c4b26687db07a632808a1bb6b83dbb38b47854a
George <gpc91@users.noreply.github.com>
2021-08-07 10:06:36 +0100

    Add project files.

---
+++ LeetCode/Problems/Medium/Solution.cs
@@ -0,0 +1,84 @@
+﻿/*
+ * Problem Set: https://leetcode.com/problems/add-two-numbers
+ * Result:      https://leetcode.com/submissions/detail/534608869/
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems.Medium.AddTwoNumbers
+{
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+
+        // Custom ListNode Builder
+        public static ListNode Build(int value, params int[] values)
+        {
+            ListNode root = new ListNode(value);
+            ListNode node = root;
+            foreach (int val in values)
+            {
+                node.next = new ListNode(val);
+                node = node.next;
+            }
+            return root;
+        }
+
+        // Custom ListNode Printer
+        public void Print()
+        {
+            ListNode ptr = this;
+            while (ptr != null)
+            {
+                Console.Write(ptr.val);
+                ptr = ptr.next;
+            }
+            Console.WriteLine();
+        }
+
+    }
+
+    public class Solution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode root = new ListNode();
+            ListNode current = root;
+
+            while (true)
+            {
+                current.val +=
+                    (l1 != null ? l1.val : 0)
+                +
+                    (l2 != null ? l2.val : 0);
+
+                if (l1 != null) l1 = l1.next;
+                if (l2 != null) l2 = l2.next;
+
+                // if there is no carry left over and we have reached the end of both lists then break out of while loop.
+                if (l1 == null && l2 == null && (current.val / 10) == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    // create a new node with the remainder of sum over 10.
+                    current.next = new ListNode(current.val / 10);
+                    // set the value to the quotient of the sum of 10 to keep it correctly clamped after the carry
+                    current.val %= 10;
+                    current = current.next;
+                }
+            }
+            return root;
+        }
+       
+    }
+}
---
+++ LeetCode/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using LeetCode.Problems.Medium.AddTwoNumbers;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ListNode l1 = ListNode.Build(2, 3, 4);
+            l1.Print();
+
+            ListNode l2 = ListNode.Build(5, 6, 8);
+            l2.Print();
+
+            Solution solution = new Solution();
+            ListNode result = solution.AddTwoNumbers(l1, l2);
+            result.Print();
+        }
+    }
+}
